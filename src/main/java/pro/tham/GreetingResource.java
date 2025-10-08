b3f28927ad48ae303801cadbff9ab9d3c11efe4d
Tham <zarub2k@gmail.com>
2025-10-08 18:49:42 +0530

    initial changes

---
+++ src/main/java/pro/tham/GreetingResource.java
@@ -0,0 +1,16 @@
+package pro.tham;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/ping")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String ping() {
+        return "Hello from Quarkus LangChain4J";
+    }
+}
